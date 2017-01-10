1044fa2a791cf6e726bf5b29ebc038d1babf2728
tamarke <tamarkrm@gmail.com>
2017-01-10 18:26:32 +0200

    GREEN:4 passing

--- cash.js
+++ cash.js
@@ -1,9 +1,9 @@
-var C = {};                    // C Object simplifies exporting the module
+/*var C = {};                    // C Object simplifies exporting the module
 C.getChange = function () {    // enough to satisfy the test
     'use strict';
     return true;               // also passes JSLint
 };
-module.exports = C;            // export the module with a single method
+
 C.getChange = function (totalPayable, cashPaid) {
     'use strict';
     return [50, 20, 20];    // just enough to pass :-)
@@ -15,8 +15,9 @@ C.getChange = function (totalPayable, cashPaid) {
         return [500, 10, 2, 2];
     else if(totalPayable == 210 && cashPaid == 300)
         return [50, 20, 20];
-};
+};*/
 var C = {};     // C Object simplifies exporting the module
+module.exports = C;            // export the module with a single method
 C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 C.getChange = function (totalPayable, cashPaid) {
     'use strict';
